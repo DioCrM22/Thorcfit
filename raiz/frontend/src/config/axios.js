91108a247252310694027d6b500af52e44782bec
Diogo <diogocrmaia03@gmail.com>
2025-05-22 12:15:18 -0300

    Remover a pasta frontend

--- raiz/frontend/src/config/axios.js
+++
@@ -1,9 +0,0 @@
-// src/config/axios.js
-import axios from "axios";
-
-axios.defaults.baseURL = "http://localhost:5000";
-axios.interceptors.request.use(config => {
-  const token = localStorage.getItem("token");
-  if (token) config.headers.Authorization = `Bearer ${token}`;
-  return config;
-});
